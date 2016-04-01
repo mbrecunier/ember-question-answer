aba3c2d2af006538b49cedf08920647863863379
Michelle Brecunier <michelle.brecunier@gmail.com>
2016-03-31 17:41:36 -0700

    add missing comma

--- app/models/answer.js
+++ app/models/answer.js
@@ -2,6 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   author: DS.attr(),
-  content: DS.attr()
+  content: DS.attr(),
   entry: DS.belongsTo('question', {async: true})
 });
